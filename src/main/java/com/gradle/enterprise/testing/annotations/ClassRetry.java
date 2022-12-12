d8ba7e6475da7d5ec0305489c3a03de2c1d8accb
Leonard Brünings <leonard.bruenings@gradle.com>
2022-12-12 17:58:41 +0100

    Add @ClassRetry annotation

---
+++ src/main/java/com/gradle/enterprise/testing/annotations/ClassRetry.java
@@ -0,0 +1,43 @@
+/*
+ *
+ *  * Copyright 2022-2022 the original author or authors.
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ *
+ */
+
+package com.gradle.enterprise.testing.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation to indicate that a test should be retried on the class level.
+ * <p>
+ *     This annotation does not imply {@link Retryable} unless configured explicitly.
+ * <p>
+ * Works only in conjunction with the test <a href="https://github.com/gradle/test-retry-gradle-plugin">test-retry-gradle-plugin</a>.
+ *
+ * @since 1.1
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ClassRetry {
+    /**
+     * Optional reason for the usage. Purely for informational purposes.
+     */
+    String because() default "";
+}
