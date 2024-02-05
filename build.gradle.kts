904ae64f728535f0997a1935d457ee904a6eda0b
Julian Honnen <208302+jhonnen@users.noreply.github.com>
2024-02-05 08:10:30 +0100

    downgrade plugin for Java 8 support
    Signed-off-by: Julian Honnen <208302+jhonnen@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     `maven-publish`
     signing
     id("io.github.gradle-nexus.publish-plugin")
-    id("biz.aQute.bnd.builder") version "7.0.0"
+    id("biz.aQute.bnd.builder") version "6.4.0"
 }
 
 java {
