9f4810649306fc3ad5d7fa10b7ed466359f20a5a
Pavlo Shevchenko <pshevchenko@gradle.com>
2024-04-04 11:52:05 +0200

    Upgrade Develocity plugin to 3.17

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 pluginManagement {
     plugins {
-        id("com.gradle.enterprise") version "3.16.2"
+        id("com.gradle.develocity") version "3.17"
         id("com.gradle.common-custom-user-data-gradle-plugin") version "1.13"
         id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
     }
@@ -8,7 +8,7 @@ pluginManagement {
 
 
 plugins {
-    id("com.gradle.enterprise")
+    id("com.gradle.develocity")
     id("com.gradle.common-custom-user-data-gradle-plugin")
 }
 
@@ -19,3 +19,9 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "develocity-testing-annotations"
+
+develocity {
+    buildScan {
+        publishing.onlyIf { false }
+    }
+}
