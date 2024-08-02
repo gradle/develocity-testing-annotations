4282ac3116b07ee0c35d61af5c333fd2da0eb667
Gasper Kojek <gkojek@gradle.com>
2024-08-02 13:30:00 +0200

    Migrated to CCUD

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,20 +1,46 @@
 pluginManagement {
     plugins {
-        id("com.gradle.enterprise") version "3.17.6"
-        id("com.gradle.enterprise.gradle-enterprise-conventions-plugin") version "0.7.5"
+        id("com.gradle.develocity") version "3.17.6"
+        id("com.gradle.common-custom-user-data-gradle-plugin") version "2.0.2"
         id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
     }
 }
 
 
 plugins {
-    id("com.gradle.enterprise")
-    id("com.gradle.enterprise.gradle-enterprise-conventions-plugin")
+    id("com.gradle.develocity")
+    id("com.gradle.common-custom-user-data-gradle-plugin")
 }
 
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
+    }
+}
+
+val isCI = providers.environmentVariable("CI").isPresent
+
+develocity {
+    server = "https://ge.gradle.org"
+    buildScan {
+        uploadInBackground = !isCI
+        publishing.onlyIf { it.isAuthenticated }
+        obfuscation {
+            ipAddresses { addresses -> addresses.map { "0.0.0.0" } }
+        }
+    }
+}
+
+buildCache {
+    local {
+        isEnabled = true
+    }
+
+    remote(develocity.buildCache) {
+        server = "https://eu-build-cache.gradle.org"
+        isEnabled = true
+        val accessKey = System.getenv("DEVELOCITY_ACCESS_KEY")
+        isPush = isCI && !accessKey.isNullOrEmpty()
     }
 }
 
