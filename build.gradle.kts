c3b3c124b97700db848036a904ea9d9dbb30dee5
bot-gh-dv-shared <bot-gh-dv-shared+renovate@gradle.com>
2024-11-28 02:07:36 -0600

    [Renovate Bot] Update plugin biz.aQute.bnd.builder to v7.1.0 (#46)
    Signed-off-by: Renovate Bot for Gradle <bot-gh-dv-shared+renovate@gradle.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     `maven-publish`
     signing
     id("io.github.gradle-nexus.publish-plugin")
-    id("biz.aQute.bnd.builder") version "7.0.0"
+    id("biz.aQute.bnd.builder") version "7.1.0"
 }
 
 java {
